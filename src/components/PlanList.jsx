92bed52d28a49515d34d1451bfcb3514f67ad5a2
F <thanetpol82@gmail.com>
2024-11-12 17:29:12 +0700

    Update

--- src/components/PlanList.jsx
+++ src/components/PlanList.jsx
@@ -1,7 +1,281 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import { IoIosArrowRoundForward } from "react-icons/io";
 
 export default function PlanList() {
   return (
-    <div>PlanList</div>
-  )
+    <div className="flex bg-[#E9EFEC] h-[100vh]">
+      <Sidebar />
+      <div className="flex flex-col w-screen mr-2 ml-2">
+        <Navbar />
+        <div className="flex-1 flex-col overflow-x-auto flex-grow p-2 bg-white mt-2 rounded-md">
+          <h1 className="text-2xl font-bold mt-3 text-center">Plan List</h1>
+          <hr className="my-6 h-0.5 bg-gray-500 opacity-100 dark:opacity-50 border-y-[1px] border-gray-300" />
+
+          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-2 mb-2">
+            <div className="flex flex-col space-y-1 relative">
+              <label className="text-xs font-bold">Search_Type</label>
+              <div className="relative w-full lg:w-60 xl:w-44">
+                <input
+                  type="text"
+                  className="bg-[#ffff99] border-solid border-2 border-gray-500 rounded-md py-0.5 w-full"
+                />
+                <select className="absolute inset-y-0 right-0 opacity-0 cursor-pointer w-full">
+                  <option value="" disabled hidden>
+                    Select
+                  </option>
+                  <option value="0">0</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                </select>
+                <div className="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
+                  <svg
+                    className="w-4 h-4 text-gray-500"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M19 9l-7 7-7-7"
+                    />
+                  </svg>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col space-y-1 relative">
+              <label className="text-xs font-bold">Delivery1</label>
+              <div className="relative w-full lg:w-60 xl:w-44">
+                <input
+                  type="text"
+                  className="bg-[#ffff99] border-solid border-2 border-gray-500 rounded-md py-0.5 w-full"
+                />
+                <select className="absolute inset-y-0 right-0 opacity-0 cursor-pointer w-full">
+                  <option value="" disabled hidden>
+                    Select
+                  </option>
+                  <option value="0">0</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                </select>
+                <div className="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
+                  <svg
+                    className="w-4 h-4 text-gray-500"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M19 9l-7 7-7-7"
+                    />
+                  </svg>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col space-y-1 relative">
+              <label className="text-xs font-bold">Delivery2</label>
+              <div className="relative w-full lg:w-60 xl:w-44">
+                <input
+                  type="text"
+                  className="bg-[#ffff99] border-solid border-2 border-gray-500 rounded-md py-0.5 w-full"
+                />
+                <select className="absolute inset-y-0 right-0 opacity-0 cursor-pointer w-full">
+                  <option value="" disabled hidden>
+                    Select
+                  </option>
+                  <option value="0">0</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                </select>
+                <div className="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
+                  <svg
+                    className="w-4 h-4 text-gray-500"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M19 9l-7 7-7-7"
+                    />
+                  </svg>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col space-y-1 relative">
+              <label className="text-xs font-bold">Delivery3</label>
+              <div className="relative w-full lg:w-60 xl:w-44">
+                <input
+                  type="text"
+                  className="bg-[#ffff99] border-solid border-2 border-gray-500 rounded-md py-0.5 w-full"
+                />
+                <select className="absolute inset-y-0 right-0 opacity-0 cursor-pointer w-full">
+                  <option value="" disabled hidden>
+                    Select
+                  </option>
+                  <option value="0">0</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                </select>
+                <div className="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
+                  <svg
+                    className="w-4 h-4 text-gray-500"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M19 9l-7 7-7-7"
+                    />
+                  </svg>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col space-y-1 relative">
+              <label className="text-xs font-bold">View_Schedule</label>
+              <div className="relative w-full lg:w-60 xl:w-44">
+                <input
+                  type="text"
+                  className="bg-[#ffff99] border-solid border-2 border-gray-500 rounded-md py-0.5 w-full"
+                />
+                <select className="absolute inset-y-0 right-0 opacity-0 cursor-pointer w-full">
+                  <option value="" disabled hidden>
+                    Select
+                  </option>
+                  <option value="0">0</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                </select>
+                <div className="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
+                  <svg
+                    className="w-4 h-4 text-gray-500"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M19 9l-7 7-7-7"
+                    />
+                  </svg>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col space-y-1 relative">
+              <label className="text-xs font-bold">Plan_Target</label>
+              <div className="relative w-full lg:w-60 xl:w-44">
+                <input
+                  type="text"
+                  className="bg-[#ffff99] border-solid border-2 border-gray-500 rounded-md py-0.5 w-full"
+                />
+                <select className="absolute inset-y-0 right-0 opacity-0 cursor-pointer w-full">
+                  <option value="" disabled hidden>
+                    Select
+                  </option>
+                  <option value="0">0</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                </select>
+                <div className="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
+                  <svg
+                    className="w-4 h-4 text-gray-500"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M19 9l-7 7-7-7"
+                    />
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <hr className="my-6 h-0.5 border-t-0 bg-gray-500 opacity-100 dark:opacity-50 border-y-[1px] border-gray-300" />
+
+          <div className="bg-white p-3 mt-5">
+            <div className="grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 gap-4">
+              <div className="grid grid-cols-4 gap-2">
+                <button className="bg-blue-500 p-3 rounded-lg hover:bg-blue-700 font-medium text-white">
+                  Search <br />
+                  検索 (F1)
+                </button>
+                <button className="bg-blue-500 p-3 rounded-lg hover:bg-blue-700 font-medium text-white">
+                  Setting <br />
+                  設定 (F2)
+                </button>
+                <button className="bg-blue-500 p-3 rounded-lg hover:bg-blue-700 font-medium text-white">
+                  Show <br />
+                  照会 (F3)
+                </button>
+                <button className="bg-blue-500 p-3 rounded-lg hover:bg-blue-700 font-medium text-white">
+                  Target <br />
+                  対象 (F4)
+                </button>
+              </div>
+              <div className="grid grid-cols-4 gap-2">
+                <button className="bg-blue-500 p-3 rounded-lg hover:bg-blue-700 font-medium text-white">
+                  Product <br />
+                  部門 (F5)
+                </button>
+                <button className="bg-blue-500 p-3 rounded-lg hover:bg-blue-700 font-medium text-white">
+                  Calc <br />
+                  生産 (F6)
+                </button>
+                <button className="bg-blue-500 p-3 rounded-lg hover:bg-blue-700 font-medium text-white">
+                  List <br />一 覽 (F7)
+                </button>
+                <button className="bg-blue-500 p-3 rounded-lg hover:bg-blue-700 font-medium text-white">
+                  Data <br />
+                  データ (F8)
+                </button>
+              </div>
+              <div className="grid grid-cols-4 gap-2">
+                <button className="bg-blue-500 p-3 rounded-lg hover:bg-blue-700 font-medium text-white">
+                  <label className="flex justify-center items-center">
+                    <IoIosArrowRoundForward className="font-medium text-2xl" />
+                    CSV
+                  </label>
+                  (F9)
+                </button>
+                <button className="bg-blue-500 p-3 rounded-lg hover:bg-blue-700 font-medium text-white">
+                  (F10)
+                </button>
+                <button className="bg-blue-500 p-3 rounded-lg hover:bg-blue-700 font-medium text-sm text-white">
+                  Clear <br />
+                  クリア (F11)
+                </button>
+                <button className="bg-blue-500 p-3 rounded-lg hover:bg-blue-700 font-medium text-white">
+                  Exit <br />
+                  終了 (F12)
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
