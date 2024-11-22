138414dc80c31a86a9edf01a1c317854dd138df5
F <thanetpol82@gmail.com>
2024-11-22 13:44:27 +0700

    Add Master menu page and add route

---
+++ src/components/Master/Coating.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Navbar from "../Navbar";
+import Sidebar from "../Sidebar";
+
+export function Coating() {
+  return (
+    <div className="flex bg-[#E9EFEC] h-[100vh]">
+      <Sidebar />
+      <div className="flex flex-col w-full mr-2 ml-2">
+        <Navbar />
+        <div className="flex-1 flex-col p-2 overflow-x-auto">
+          <div className="bg-white grid grid-cols-1">
+            <div className="flex justify-center items-center">
+              <h1 className="text-2xl font-bold text-center">
+                Coating <br /> （CVD･PVD･DLC）
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/Master/CustomerPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Customer } from '../../components/Master/Customer'
+
+export function CustomerPage() {
+  return (
+    <Customer/>
+  )
+}
+
