bb7d169f1c8e6102b2b2f18bf20c7c7aeb8aada8
wazwatt_dell <tamkmutnb@gmail.com>
2025-01-09 15:15:25 +0700

    feat(config): set up Vite server port configuration with env variables
    - Added dotenv support for server configuration
    - Set default port to 5173 with environment variable override
    - Added host configuration for development server

--- src/configs/env.js
+++ src/configs/env.js
@@ -1,1 +1,5 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
 export const BACKEND_URL = "http://localhost:4000";
+export const ENVJS_PORT = process.env.VITE_PORT
+// export const VITE_PORT = import.meta.env.VITE_PORT || 5173;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ENVJS_PORT } from './src/configs/env.js'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0", // or 'localhost'
+    port: parseInt(ENVJS_PORT) || 5173
+  },
 })
